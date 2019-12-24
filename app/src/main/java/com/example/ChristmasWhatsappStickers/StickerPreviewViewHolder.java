d20ce278e30fc41ae44560993ff9c705e4e08559
Yash Parsana <yashparsana555@gmail.com>
2019-12-24 15:49:13 +0530

    Added files

---
+++ app/src/main/java/com/example/ChristmasWhatsappStickers/StickerPreviewViewHolder.java
@@ -0,0 +1,17 @@
+package com.example.ChristmasWhatsappStickers;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+
+class StickerPreviewViewHolder extends RecyclerView.ViewHolder {
+
+    final SimpleDraweeView stickerPreviewView;
+
+    StickerPreviewViewHolder(final View itemView) {
+        super(itemView);
+        stickerPreviewView = itemView.findViewById(R.id.sticker_preview);
+    }
+}
